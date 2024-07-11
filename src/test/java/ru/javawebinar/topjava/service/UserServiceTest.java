2b128583e749a0159533d33122d28c67bc34bdd2
garycor260176 <ipolnikov@gmail.com>
2024-07-11 20:10:30 +0300

    HW4 without optional

--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -23,16 +23,7 @@ import java.util.List;
 import static org.junit.Assert.assertThrows;
 import static ru.javawebinar.topjava.UserTestData.*;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-//@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
-@ActiveProfiles(profiles = {Profiles.DATAJPA, Profiles.POSTGRES_DB})
-public class UserServiceTest {
-
+public abstract class UserServiceTest extends AbstractServiceTest {
     @Autowired
     private UserService service;
 
