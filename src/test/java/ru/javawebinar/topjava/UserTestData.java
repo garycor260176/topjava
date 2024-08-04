815f60f91e9b072073d06aa2280f7b8ce4ef967a
garycor260176 <ipolnikov@gmail.com>
2024-08-05 00:57:12 +0300

    HW07 + optionals

--- src/test/java/ru/javawebinar/topjava/UserTestData.java
+++ src/test/java/ru/javawebinar/topjava/UserTestData.java
@@ -10,6 +10,7 @@ import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class UserTestData {
     public static final MatcherFactory.Matcher<User> USER_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(User.class, "registered", "meals");
+    public static final MatcherFactory.Matcher<User> USER_WITH_MEALS_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(User.class, "registered");
 
     public static final int USER_ID = START_SEQ;
     public static final int ADMIN_ID = START_SEQ + 1;
@@ -19,6 +20,17 @@ public class UserTestData {
     public static final User user = new User(USER_ID, "User", "user@yandex.ru", "password", Role.USER);
     public static final User admin = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ADMIN, Role.USER);
     public static final User guest = new User(GUEST_ID, "Guest", "guest@gmail.com", "guest");
+
+    public static final User userWithMeals;
+    public static final User adminWithMeals;
+
+    static {
+        userWithMeals = new User(user);
+        userWithMeals.setMeals(MealTestData.meals);
+
+        adminWithMeals = new User(admin);
+        adminWithMeals.setMeals(MealTestData.adminMeals);
+    }
 
     public static User getNew() {
         return new User(null, "New", "new@gmail.com", "newPass", 1555, false, new Date(), Collections.singleton(Role.USER));
